ade68233abb6cfd9f90319a6d7298d0808a8d2fd
ybinh07 <yenbinh18082307@gmail.com>
2023-10-30 21:25:19 +0700

    hieu

--- YenBinh/vite.config.js
+++ YenBinh/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import InjectCSS from '@itsy/vite-css-inject';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    InjectCSS(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@tests": path.resolve(__dirname, "./tests")
+    }
+  }
 })
